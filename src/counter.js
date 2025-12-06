aa31568193f4ddc5de38e5bed8e941a7dac989e0
=kaptenPijay <=muhammadzahran0205@gmail.com>
2025-12-06 20:51:35 +0700

    deposito clear

--- src/counter.js
+++ src/counter.js
@@ -1,89 +1,22 @@
 // counter.js
 
-// Fungsi cekNilai
-let ekl = 0;
-export function cekNilai(tipe, tahun, ekalloyd, rules) {
-    const t = tipe.value;
-    const th = tahun.value;
-
-    if (rules[t] && rules[t][th]) {
-        ekalloyd.value = rules[t][th];
-        ekl = rules[t][th];
-    } else {
-        ekalloyd.value = "";
-    }
-}
-
-// Fungsi PMT
-export function PMT(rate, nper, pv) {
-    return (rate * pv) / (1 - Math.pow(1 + rate, -nper));
-}
-
-// Fungsi hitung cicilan
-export function hitungCicilan(plafond, tahun, blokir,new_angsuran) {
-    const bungaTahunan = 0.1225;
-    const bungaBulanan = bungaTahunan / 12;
-    const pokokPinjaman = Number(plafond.dataset.value || 0);
-    const tenorBulan = tahun.value * 12;
-
-    const hasil = PMT(bungaBulanan, tenorBulan, -pokokPinjaman);
-
-    blokir.value = Math.abs(Math.round(hasil)).toLocaleString("id-ID");
-    new_angsuran.value = Math.abs(Math.round(hasil)).toLocaleString("id-ID");
-}
-
 // Fungsi replaceDot (format input ribuan)
-export function replaceDot(plafond) {
-    let angka = plafond.value.replace(/\D/g, "");
-    plafond.dataset.value = angka;
-    plafond.value = angka ? Number(angka).toLocaleString("id-ID") : "";
+export function replaceDot(nominal) {
+    let angka = nominal.value.replace(/\D/g, "");
+    nominal.dataset.value = angka;
+    nominal.value = angka ? Number(angka).toLocaleString("id-ID") : "";
 }
+export function grossAmount(nominal,tenor,persentase,bungaKotor,pajak,estimasiBersih) {
+    let bunga = parseFloat(
+    (persentase.value.trim() === "" ? "3.5%" : persentase.value).replace("%", "")
+) / 100;
+    let hasil = nominal.dataset.value * bunga * tenor.value / 365;
+    const kotor = hasil ? Number(hasil) : 0;
+    const pajakVal = hasil ? Number(hasil * 0.2) : 0;
+    const bersih = kotor - pajakVal;
 
-// Fungsi hitungProvisi
-export function hitungProvisi(plafond, provisi) {
-    let nilaiPlafond = Number(plafond.dataset.value || 0);
-    let hasil = Math.round(nilaiPlafond * 0.005);
-
-    provisi.dataset.value = hasil;
-    provisi.value = hasil.toLocaleString("id-ID");
-}
-export function hitungBiayaAdmin(admin,tahun){
-  let biayaAdmin = Number(30000 * (tahun.value*12));
-  admin.value = biayaAdmin.toLocaleString("id-ID");
-}
-export function hitungBiayaAsuransi(plafond,asuransi){
-  let byAss = Number(ekl*plafond.dataset.value/1000);
-  asuransi.value = byAss.toLocaleString("id-ID");
-}
-export function totalPotongan(total, blokir=0, provisi=0, admin=0, asuransi=0) {
-    // Pastikan semua parameter berupa angka murni
-    const b = Number(String(blokir).replace(/\D/g, "")) || 0;
-    const p = Number(String(provisi).replace(/\D/g, "")) || 0;
-    const a = Number(String(admin).replace(/\D/g, "")) || 0;
-    const as = Number(String(asuransi).replace(/\D/g, "")) || 0;
-
-    // Tambah biaya tetap 250000
-    const hasil = b + p + a + as + 250000;
-
-    // Simpan angka asli tanpa titik
-    total.dataset.value = hasil;
-
-    // Tampilkan format ribuan
-    total.value = hasil.toLocaleString("id-ID");
-}
-export function sisaGaji(estimasi,newAngsuran,dat) {
-    const e = Number(String(estimasi.value).replace(/\D/g, "")) || 0;
-    const n = Number(String(newAngsuran.value).replace(/\D/g, "")) || 0;
-    let hasil = e-n;
-    dat.dataset.value = hasil;
-    dat.value = hasil.toLocaleString("id-ID")
-}
-
-export function estimasiBersihDiterima(plafond, total, estimasiBersih) {
-    const e = Number(String(plafond.value).replace(/\D/g, "")) || 0;
-    const n = Number(String(total.value).replace(/\D/g, "")) || 0;
-    let hasil = e-n;
-    estimasiBersih.dataset.value = hasil;
-    estimasiBersih.value = hasil.toLocaleString("id-ID")
+    bungaKotor.value = kotor.toLocaleString("id-ID");
+    pajak.value = pajakVal.toLocaleString("id-ID");
+    estimasiBersih.value = bersih.toLocaleString("id-ID");
 }
 
--- src/main.js
+++ src/main.js
@@ -1,125 +1,57 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { cekNilai, hitungCicilan, replaceDot, hitungProvisi, hitungBiayaAdmin, hitungBiayaAsuransi, totalPotongan, sisaGaji, estimasiBersihDiterima } from './counter.js';
+import { replaceDot, grossAmount} from './counter.js';
 import * as XLSX from 'xlsx';
 import Swal from 'sweetalert2'
 
 document.addEventListener("DOMContentLoaded",()=>{
-const tahun = document.getElementById("tahun");
-const tipe = document.getElementById("tipe_credit");
-const ekalloyd = document.getElementById("ekalloyd");
-const plafond = document.getElementById("plafond");
-const provisi = document.getElementById("provisi");
-const admin = document.getElementById("admin");
-const asuransi = document.getElementById("asuransi");
-const blokir = document.getElementById("blokir");
-const total = document.getElementById("total");
-const salary = document.getElementById("salary");
-const newAngsuran = document.getElementById("new_angsuran");
-const gajiTersisa = document.getElementById("sisa_gaji");
+const tenor = document.getElementById("tahun");
+const jenis = document.getElementById("jenis");
+const nominal = document.getElementById("nominal");
+const persentase = document.getElementById("persentase");
+const bungaKotor = document.getElementById("bungaKotor");
+const pajak = document.getElementById("pajak");
 const estimasiBersih = document.getElementById("estimasiBersih");
 
-for (let i = 1; i <= 20; i++) {
-    tahun.insertAdjacentHTML("beforeend", `
-        <option value="${i}">${i} Tahun</option>
-    `);
-}
-tipe.innerHTML = `
-                  <option value="">Tipe Kredit</option>
-                  <option value="pns">Konsumtif PNS</option>
-                  <option value="prapen">Konsumtif Prapen</option>
-                  <option value="pensiun">Konsumtif Pensiun</option>
+const pilihanTenor = [
+  { value: 30, label: "1 Bulan" },
+  { value: 90, label: "3 Bulan" },
+  { value: 180, label: "6 Bulan" },
+  { value: 365, label: "1 Tahun" }
+];
+
+tenor.innerHTML = pilihanTenor
+  .map(item => `<option value="${item.value}">${item.label}</option>`)
+  .join("");
+
+jenis.innerHTML = `
+                  <option value="">Tipe Deposito</option>
+                  <option value="deposito">Deposito</option>
+                  <option value="silah">Silah</option>
+                  <option value="doubleUntung">Double Untung</option>
 `
-const rules = {
-    "pns": {
-        "1": 2.56,
-        "2": 4.98,
-        "3": 7.02,
-        "4": 8.87,
-        "5": 10.62,
-        "6": 13.85,
-        "7": 15.53,
-        "8": 18.99,
-        "9": 20.45,
-        "10": 22.63,
-        "11": 24.36,
-        "12": 27.75,
-        "13": 30.97,
-        "14": 31.90,
-        "15": 33.58,
-        "16": 43.90,
-        "17": 47.65,
-        "18": 49.08,
-        "19": 51.37,
-        "20": 53.76,
-    },
-    "prapen": {
-        "1": 5.14,
-        "2": 9.45,
-        "3": 15.83,
-        "4": 19.39,
-        "5": 24.90,
-        "6": 27.01,
-        "7": 29.85,
-        "8": 32.11,
-        "9": 35.10,
-        "10": 38.21,
-        "11": 41.05,
-        "12": 44.38,
-        "13": 46.08,
-        "14": 48.23,
-        "15": 50.49,
-        "16": 52.60,
-        "17": 56.05,
-        "18": 58.43,
-        "19": 60.92,
-        "20": 62.88,
-    },
-    "pensiun": {
-         "1": 5.50,
-        "2": 11.00,
-        "3": 16.50,
-        "4": 22.00,
-        "5": 27.50,
-        "6": 33.00,
-        "7": 38.50,
-        "8": 44.00,
-        "9": 49.50,
-        "10": 55.00,
-        "11": 60.50,
-        "12": 66.00,
-        "13": 71.50,
-        "14": 77.00,
-        "15": 82.50,
-    }
-};
 
-// Event listener
-plafond.addEventListener("keyup", () => {
-    replaceDot(plafond);
-    hitungCicilan(plafond, tahun, blokir, newAngsuran);
-    hitungProvisi(plafond, provisi);
-    hitungBiayaAdmin(admin,tahun);
-    hitungBiayaAsuransi(plafond,asuransi);
-    totalPotongan(total,blokir.value,provisi.dataset.value,admin.value,asuransi.value)
-    estimasiBersihDiterima(plafond,total,estimasiBersih)
-});
 
-tahun.addEventListener("change", () => {
-    cekNilai(tipe, tahun, ekalloyd, rules);
-    hitungCicilan(plafond, tahun, blokir, newAngsuran);
-    hitungBiayaAsuransi(plafond,asuransi);
-    hitungBiayaAdmin(admin,tahun);
-    totalPotongan(total,blokir.value,provisi.dataset.value,admin.value,asuransi.value)
-    estimasiBersihDiterima(plafond,total,estimasiBersih)
-});
-tipe.addEventListener("change", () => {
-  cekNilai(tipe, tahun, ekalloyd, rules)
-  hitungBiayaAsuransi(plafond,asuransi);
-  totalPotongan(total,blokir.value,provisi.dataset.value,admin.value,asuransi.value)
-  estimasiBersihDiterima(plafond,total,estimasiBersih)
-});
-salary.addEventListener("keyup",()=>{
-    replaceDot(salary);
-    sisaGaji(salary,newAngsuran,gajiTersisa)
+nominal.addEventListener('keyup',()=>{
+    replaceDot(nominal);
+    grossAmount(nominal,tenor,persentase,bungaKotor,pajak,estimasiBersih)
 })
+tenor.addEventListener('change',()=>{
+    grossAmount(nominal,tenor,persentase,bungaKotor,pajak,estimasiBersih)
+})
+persentase.addEventListener("input", function () {
+  let value = this.value.replace("%", "");
+  if (value === "") {
+    this.value = "";
+    return;
+  }
+  this.value = value + "%";
+  grossAmount(nominal,tenor,persentase,bungaKotor,pajak,estimasiBersih)
+});
+persentase.addEventListener("keydown", function (e) {
+  if (e.key === "Backspace" && this.value.endsWith("%")) {
+    this.value = this.value.replace("%", "");
+  }
+  grossAmount(nominal,tenor,persentase,bungaKotor,pajak,estimasiBersih)
+});
+
 })
